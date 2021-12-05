df20be4b3fd1c78b8322846906ec6ae71f36c398
Rosa, Genesis <mrgenesis@hotmail.com>
2021-12-05 07:29:25 -0400

    adiciona opcao para definir propriedades personalizadas no arquivo de configuração

--- app.config.js
+++ app.config.js
@@ -3,6 +3,19 @@
 const ROOT_PROJECT = __dirname;
 
 function appConfig() {
+  const path = require('path')
+      , fs = require('fs')
+      , Types = this.tools.types();
+  let customConfig = {};
+
+  const CUSTOM_CONFIG_FILE_NAME = process.env.CUSTOM_CONFIG_FILE_NAME;
+  if (Types.isString(CUSTOM_CONFIG_FILE_NAME)) {
+   const configFilePath = path.resolve(CUSTOM_CONFIG_FILE_NAME);
+    if(fs.existsSync(configFilePath)) {
+      customConfig = require(configFilePath);
+    }
+  }
+
   return {
     "databases": {
       "mongodb": {
@@ -15,7 +28,8 @@ function appConfig() {
       }
     },
     "rootProject": ROOT_PROJECT,
-    "modulesFolder": `${ROOT_PROJECT}/modules`
+    "modulesFolder": `${ROOT_PROJECT}/modules`,
+    ...customConfig
   }
 }
 
