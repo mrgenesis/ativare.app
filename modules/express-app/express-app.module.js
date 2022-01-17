d332e90397dd70da453bcfd59e040d3bc7b3261e
Rosa, Genesis <mrgenesis@hotmail.com>
2022-01-17 07:08:23 -0400

    Adicionado middleware auth para lidar com autenticações

--- modules/express-app/express-app.module.js
+++ modules/express-app/express-app.module.js
@@ -1,16 +1,24 @@
 'use strict';
 
 class ExpressApp {
+  #app;
+  #context;
   #express = require('express');
   constructor(context) {
-    this.context = context;
-    this.app = this.#express();
-    this.app.use(this.#express.json());
-    this.app.use(this.#express.urlencoded({ extended: false }));
+    this.#context = context;
+    this.#app = this.#express();
+    this.#app.use(this.#express.json());
+    this.#app.use(this.#express.urlencoded({ extended: false }));
+    this.setMiddlewares();
   }
 
   getApp() {
-    return this.app;
+    return this.#app;
+  }
+  setMiddlewares() {
+    const { Middleware } = this.#context;
+    const auth = Middleware.getMiddleware({ middlewareName: 'auth' });
+    this.#app.use(auth);
   }
 
 
