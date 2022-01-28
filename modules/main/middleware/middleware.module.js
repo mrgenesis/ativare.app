15b89a5dc872ed085e32a74fe23f0ef0203b17db
Rosa, Genesis <mrgenesis@hotmail.com>
2022-01-28 17:39:34 -0400

    O module interno middleware agora diferencia os que serão executado no contexto global e os que serão executado no contexto local

--- modules/main/middleware/middleware.module.js
+++ modules/main/middleware/middleware.module.js
@@ -1,26 +1,36 @@
 'use strict';
 
 class Middleware {
-  #middlewares = {};
+  #globalMiddlewares = {};
+  #moduleMiddlewares = {};
+  #context;
   constructor(context) {
-    const { Helper } = context;
-    debugger;
-    this.#middlewares = Helper
-      .getFiles(__dirname)
-      .ignore([require('path').basename(__filename)])
-      .requireAll()
-      .runAll(context);
+    this.#context = context;
+    this.loadMiddlewares();
+  }
+  loadMiddlewares() {
+    const { Helper } = this.#context;
+    const Types = Helper.types();
+    const filesList = Helper.getFiles(__dirname).ignore([require('path').basename(__filename)]).list();
+    filesList.forEach(file => {
+      const middleware = require(`./${file}`)(this.#context);
+      const loader = {
+        global: () => this.#globalMiddlewares[middleware.name] = middleware,
+        module: () => this.#moduleMiddlewares[middleware.name] = middleware
+      };
+      if(Types.isNotFunction(loader[middleware.type])) {
+        throw 'The property "type" is mandatory. And should be a string their "global" or "module".';
+      }
+      loader[middleware.type]();
+    });
+  }
+  
+  getGlobalMiddlewares() {
+    return Object.values(this.#globalMiddlewares);
   }
 
-  getMiddleware({ middlewareName, resource, currenteModule }) {
-    return this.#middlewares[middlewareName](resource, currenteModule);
-  }
-  getMiddlewares({ middlewares, resource, currenteModule }) {
-    return middlewares.map(middleware => {
-      if(this.#middlewares[middleware]) {
-        return this.#middlewares[middleware](resource, currenteModule);
-      }
-    });
+  getModuleMiddleware(middlewareName) {
+    return this.#moduleMiddlewares[middlewareName];
   }
 }
 
