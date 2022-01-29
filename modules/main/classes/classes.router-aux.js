cd3d3fdb109b4ffe3651845ba379b40fd583764e
Rosa, Genesis <mrgenesis@hotmail.com>
2022-01-29 17:26:17 -0400

    Class que extende todos os módulos do tipo route.

---
+++ modules/main/classes/classes.router-aux.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class RouterAux {
+  #folderModule; #resource;
+  constructor({ folderPath, context }) {
+    this.#folderModule = folderPath;
+
+    const { ResourceManager } = context.Classes;
+    const Manager = new ResourceManager({ module: this, folderPath, context });    
+    this.setResource(Manager.getResourcesAll());
+  }
+  get name() {
+    return this.constructor.name;
+  }
+  get lowerCaseName() {
+    return this.name.toLowerCase();
+  }
+  get entry() {
+    return `/${this.lowerCaseName}`;
+  }
+  setResource(resource) {
+    this.#resource = resource;
+  }
+  getRouter() {
+    return require(`${this.#folderModule}/${this.name}.controller`)(this.#resource);
+  }
+  getResourcesAll() {
+    return this.#resource;
+  }
+};
+
+module.exports = RouterAux;
+
