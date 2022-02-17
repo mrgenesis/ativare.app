e2e82ea68729f5775058b884c924f9bacbb19021
Rosa, Genesis <mrgenesis@hotmail.com>
2022-02-17 16:57:14 -0400

    o aruqivo de teste foi atualizado para uma cópia do que obj gerado pelo front

---
+++ modules/budget/budget-generator/fixtures/budget-front.js
@@ -0,0 +1,300 @@
+module.exports = {
+  "customer": {
+      "phone": "33333333333",
+      "name": "doe",
+      "email": "e@e"
+  },
+  "productsList": [
+      {
+          "productCode": "P7",
+          "_id": "61fbe6b8438a7207f4f0eb55",
+          "name": "Iluminação Dimmerizada",
+          "description": "Potência 160W/127V ",
+          "materials": [
+              {
+                  "code": 52,
+                  "_id": "61fbe4a5438a7207f4f0eb48",
+                  "name": "Mini Dimmer",
+                  "charge": 1
+              }
+          ],
+          "category": "Automação",
+          "group": "Iluminação",
+          "floor": "2º Pavimento",
+          "homeLocationName": "suite master",
+          "amount": 3,
+          "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "10",
+              "pulser": "25",
+              "floor": "2º Pavimento",
+              "locationName": "suite master"
+          }
+      },
+      {
+          "productCode": "P8",
+          "_id": "61fbe745438a7207f4f0eb59",
+          "name": "Iluminação L/D",
+          "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+          "materials": [
+              {
+                  "code": 52,
+                  "_id": "61fbe4a5438a7207f4f0eb48",
+                  "name": "Mini Dimmer",
+                  "charge": 1
+              },
+              {
+                  "code": 53,
+                  "_id": "61fbe4ef438a7207f4f0eb4b",
+                  "name": "Expansão 216",
+                  "charge": 1
+              }
+          ],
+          "category": "Automação",
+          "group": "Iluminação",
+          "floor": "2º Pavimento",
+          "homeLocationName": "suite master",
+          "amount": 3,
+          "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "10",
+              "pulser": "25",
+              "floor": "2º Pavimento",
+              "locationName": "suite master"
+          }
+      },
+      {
+          "productCode": "P9",
+          "_id": "61fbe857438a7207f4f0eb5f",
+          "name": "Persianas Motorizadas",
+          "description": "Persianas ",
+          "materials": [
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 1
+              },
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 0.5
+              }
+          ],
+          "category": "Automação",
+          "group": "Acessórios",
+          "floor": "2º Pavimento",
+          "homeLocationName": "suite master",
+          "amount": 3,
+          "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "10",
+              "pulser": "25",
+              "floor": "2º Pavimento",
+              "locationName": "suite master"
+          }
+      },
+      {
+          "productCode": "P9",
+          "_id": "61fbe857438a7207f4f0eb5f",
+          "name": "Persianas Motorizadas",
+          "description": "Persianas ",
+          "materials": [
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 1
+              },
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 0.5
+              }
+          ],
+          "category": "Automação",
+          "group": "Acessórios",
+          "floor": "Térreo",
+          "homeLocationName": "quarto",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "2",
+              "pulser": "6",
+              "floor": "Térreo",
+              "locationName": "quarto"
+          }
+      },
+      {
+          "productCode": "P9",
+          "_id": "61fbe857438a7207f4f0eb5f",
+          "name": "Persianas Motorizadas",
+          "description": "Persianas ",
+          "materials": [
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 1
+              },
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 0.5
+              }
+          ],
+          "category": "Automação",
+          "group": "Acessórios",
+          "floor": "Térreo",
+          "homeLocationName": "sala",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "20",
+              "floor": "Térreo",
+              "locationName": "sala"
+          }
+      },
+      {
+          "productCode": "P9",
+          "_id": "61fbe857438a7207f4f0eb5f",
+          "name": "Persianas Motorizadas",
+          "description": "Persianas ",
+          "materials": [
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 1
+              },
+              {
+                  "code": 54,
+                  "_id": "61fbe5dc438a7207f4f0eb4e",
+                  "name": "Expansão 212/24V",
+                  "charge": 0.5
+              }
+          ],
+          "category": "Automação",
+          "group": "Acessórios",
+          "floor": "Térreo",
+          "homeLocationName": "suite",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "6",
+              "pulser": "12",
+              "floor": "Térreo",
+              "locationName": "suite"
+          }
+      },
+      {
+          "productCode": "P8",
+          "_id": "61fbe745438a7207f4f0eb59",
+          "name": "Iluminação L/D",
+          "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+          "materials": [
+              {
+                  "code": 52,
+                  "_id": "61fbe4a5438a7207f4f0eb48",
+                  "name": "Mini Dimmer",
+                  "charge": 1
+              },
+              {
+                  "code": 53,
+                  "_id": "61fbe4ef438a7207f4f0eb4b",
+                  "name": "Expansão 216",
+                  "charge": 1
+              }
+          ],
+          "category": "Automação",
+          "group": "Iluminação",
+          "floor": "Térreo",
+          "homeLocationName": "suite",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "6",
+              "pulser": "12",
+              "floor": "Térreo",
+              "locationName": "suite"
+          }
+      },
+      {
+          "productCode": "P8",
+          "_id": "61fbe745438a7207f4f0eb59",
+          "name": "Iluminação L/D",
+          "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+          "materials": [
+              {
+                  "code": 52,
+                  "_id": "61fbe4a5438a7207f4f0eb48",
+                  "name": "Mini Dimmer",
+                  "charge": 1
+              },
+              {
+                  "code": 53,
+                  "_id": "61fbe4ef438a7207f4f0eb4b",
+                  "name": "Expansão 216",
+                  "charge": 1
+              }
+          ],
+          "category": "Automação",
+          "group": "Iluminação",
+          "floor": "Térreo",
+          "homeLocationName": "sala",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "20",
+              "floor": "Térreo",
+              "locationName": "sala"
+          }
+      },
+      {
+          "productCode": "P8",
+          "_id": "61fbe745438a7207f4f0eb59",
+          "name": "Iluminação L/D",
+          "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+          "materials": [
+              {
+                  "code": 52,
+                  "_id": "61fbe4a5438a7207f4f0eb48",
+                  "name": "Mini Dimmer",
+                  "charge": 1
+              },
+              {
+                  "code": 53,
+                  "_id": "61fbe4ef438a7207f4f0eb4b",
+                  "name": "Expansão 216",
+                  "charge": 1
+              }
+          ],
+          "category": "Automação",
+          "group": "Iluminação",
+          "floor": "Térreo",
+          "homeLocationName": "quarto",
+          "amount": 2,
+          "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "2",
+              "pulser": "6",
+              "floor": "Térreo",
+              "locationName": "quarto"
+          }
+      }
+  ]
+}
