e2bab45e7d020fefe2682d97649d4d0c06d779f2
Rosa, Genesis <mrgenesis@hotmail.com>
2022-02-17 14:31:54 -0400

    Estava retornando a string JWT. Esse retorno era inesperado no FRONt e estava gerando erro. Modificado para retornar o objeto com as propriedades authorization e user

--- modules/user/service/auth.js
+++ modules/user/service/auth.js
@@ -17,8 +17,9 @@ module.exports = function setContext(context) {
     }
     
     delete user.password;
-    
-    return authc.generateToken({ ...user });
+
+    const authorization = await authc.generateToken({ param: { ...user }});
+    return { authorization, user: { email: user.email, name: user.name } }
   }
 
   return new ServiceFactory(auth, 'w', 'Permite entrar com login de senha para visualizar as informações restritas.');
