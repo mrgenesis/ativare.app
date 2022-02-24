31f366a0df3480d9777bd7c4cdfb9551baf11c88
Rosa, Genesis <mrgenesis@hotmail.com>
2022-02-24 07:34:38 -0400

    Adicionado Subscriber para executar tarefas adicionais

---
+++ modules/main/subscriber/subscriber.module.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// const ee = new EventEmitter();
+
+class Subscriber {
+  #context;
+  constructor(context) {
+    this.#context = context;
+    const EventEmitter = require('events');
+    const ee = new EventEmitter();
+
+    ee.on('send_email', data => this.sendEmail(data));
+    ee.on('app_error', err => this.appError(err));
+    return ee;
+  }
+  sendEmail(data) {
+    console.log('Simulação do e-mail enviado. Dados:', data);
+  }
+  appError(err) {
+    const AppError = this.#context.AppError;
+    let unknowledge;
+    try {
+      if (!err.isAppError) {
+        unknowledge = AppError.unknowledge(err);
+        err.errorId = unknowledge.errorId;
+      }
+      this.#context.model.debug().create({ errorId: err.errorId, rawData: unknowledge || err });
+    } catch (e) {
+      console.log('Não não possível gravar o erro no banco de dados.', e);
+    }
+  }
+}
+
+Subscriber.priority = 9;
+module.exports = Subscriber;
