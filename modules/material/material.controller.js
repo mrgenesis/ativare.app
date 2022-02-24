2efe9d19f87502b95d6974e6df94cdd31b372bca
Rosa, Genesis <mrgenesis@hotmail.com>
2022-02-24 14:17:04 -0400

    Ajustado o código do controller do módulo Material para tratar os error apresentados.

--- modules/material/material.controller.js
+++ modules/material/material.controller.js
@@ -1,36 +1,40 @@
 'use strict';
 
-module.exports = function materialController(src) {
+module.exports = function materialController(resources) {
   const router = require('express').Router();
+  const { new_w, _r, [':materialId_r']: materialId_r, edit_u } = resources;
 
-  const { new_w, _r, [':materialId_r']: materialId_r, edit_u } = src;
-  router[new_w.method]('/new', new_w.middlewares, async function createMaterial(req, res, next) {
+  router[new_w.method](new_w.relativePath, new_w.middlewares, async function createMaterial(req, res, next) {
     try {
       const materialAdded = await new_w.service(req.body);
       res.status(201).json(materialAdded);
       
     } catch (err) {
-      console.log(err)
-      // throw MaterialModule.error.customError('Invalid data');
+      new_w.error(next, err);
     }
   });
+
   router[_r.method](_r.relativePath, async function(req, res, next) {
-    
+
     try {
       const materials = await _r.service();
       res.status(200).json(materials);
     } catch (err) {
-      console.log(err);
+      _r.error(next, err);
     }
   });
   
   router[materialId_r.method](materialId_r.relativePath, async function(req, res, next) {
     const { materialId } = req.params;
-    const material = await materialId_r.service({ code: materialId });
-    res.status(200).json(material);
+    try {
+      const material = await materialId_r.service({ code: materialId });
+      res.status(200).json(material);
+    } catch (err) {
+      materialId.error(next, err);
+    }
   });
   
-  router.post(edit_u.relativePath, async (req, res) => {
+  router.post(edit_u.relativePath, async (req, res, next) => {
     const materialUpdate = req.body;
     try {
       const set = { '$set': { ...materialUpdate } }
@@ -42,8 +46,7 @@ module.exports = function materialController(src) {
       return res.status(201).send({ ...material.toObject() });
           
     } catch (err) {
-      console.error(err);
-      res.status(400).send({ Error: 'Não foi possível processar esta solicitação' })
+      edit_u.error(next, err);
     }
   });
   
