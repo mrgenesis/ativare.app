dedc59069f6b5057a8e08a56b5e4565f0d09fd14
Rosa, Genesis <mrgenesis@hotmail.com>
2022-02-26 07:47:16 -0400

    Adicionado um orçamento mais completo para realizar os testes.

--- modules/budget/budget-generator/fixtures/budget-db.js
+++ modules/budget/budget-generator/fixtures/budget-db.js
@@ -1,7 +1,469 @@
 module.exports = {
   "_id": {
-    "$oid":"620f8f586cfc2c62bcc141a3"},
-    "productsList":[],
-    "items":
-      {"Térreo":{}},
-      "customer":{"phone":"55555555555","name":"rrr","email":"r@r"},"createAt":{"$date":"2022-02-18T12:21:44.806Z"},"code":"O1","__v":0};
+    "$oid": "620eb98a89334a3522381d76"
+  },
+  "productsList": [],
+  "items": {
+    "2º Pavimento": {
+      "suite master": {
+        "config": {
+          "I2CKeyPad": "2",
+          "multiplexedKeyPad": 0,
+          "point": "10",
+          "pulser": "25",
+          "floor": "2º Pavimento",
+          "locationName": "suite master"
+        },
+        "products": [
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "2º Pavimento",
+            "homeLocationName": "suite master",
+            "amount": 2,
+            "configLocation": {
+              "I2CKeyPad": "2",
+              "multiplexedKeyPad": 0,
+              "point": "10",
+              "pulser": "25",
+              "floor": "2º Pavimento",
+              "locationName": "suite master"
+            }
+          }
+        ]
+      },
+      "quarto": {
+        "config": {
+          "I2CKeyPad": "1",
+          "multiplexedKeyPad": 0,
+          "point": "4",
+          "pulser": "8",
+          "floor": "2º Pavimento",
+          "locationName": "quarto"
+        },
+        "products": [
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "2º Pavimento",
+            "homeLocationName": "quarto",
+            "amount": 2,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "4",
+              "pulser": "8",
+              "floor": "2º Pavimento",
+              "locationName": "quarto"
+            }
+          },
+          {
+            "productCode": "P8",
+            "_id": "61fbe745438a7207f4f0eb59",
+            "name": "Iluminação L/D",
+            "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              },
+              {
+                "code": 53,
+                "_id": "61fbe4ef438a7207f4f0eb4b",
+                "name": "Expansão 216",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "2º Pavimento",
+            "homeLocationName": "quarto",
+            "amount": 2,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "4",
+              "pulser": "8",
+              "floor": "2º Pavimento",
+              "locationName": "quarto"
+            }
+          },
+          {
+            "productCode": "P9",
+            "_id": "61fbe857438a7207f4f0eb5f",
+            "name": "Persianas Motorizadas",
+            "description": "Persianas ",
+            "materials": [
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 1
+              },
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 0.5
+              }
+            ],
+            "category": "Automação",
+            "group": "Acessórios",
+            "floor": "2º Pavimento",
+            "homeLocationName": "quarto",
+            "amount": 2,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "4",
+              "pulser": "8",
+              "floor": "2º Pavimento",
+              "locationName": "quarto"
+            }
+          }
+        ]
+      }
+    },
+    "Térreo": {
+      "quarto": {
+        "config": {
+          "I2CKeyPad": "1",
+          "multiplexedKeyPad": 0,
+          "point": "8",
+          "pulser": "15",
+          "floor": "Térreo",
+          "locationName": "quarto"
+        },
+        "products": [
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "quarto",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "15",
+              "floor": "Térreo",
+              "locationName": "quarto"
+            }
+          }
+        ]
+      },
+      "suite": {
+        "config": {
+          "I2CKeyPad": "1",
+          "multiplexedKeyPad": 0,
+          "point": "8",
+          "pulser": "15",
+          "floor": "Térreo",
+          "locationName": "suite"
+        },
+        "products": [
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "suite",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "15",
+              "floor": "Térreo",
+              "locationName": "suite"
+            }
+          },
+          {
+            "productCode": "P9",
+            "_id": "61fbe857438a7207f4f0eb5f",
+            "name": "Persianas Motorizadas",
+            "description": "Persianas ",
+            "materials": [
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 1
+              },
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 0.5
+              }
+            ],
+            "category": "Automação",
+            "group": "Acessórios",
+            "floor": "Térreo",
+            "homeLocationName": "suite",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "15",
+              "floor": "Térreo",
+              "locationName": "suite"
+            }
+          },
+          {
+            "productCode": "P8",
+            "_id": "61fbe745438a7207f4f0eb59",
+            "name": "Iluminação L/D",
+            "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              },
+              {
+                "code": 53,
+                "_id": "61fbe4ef438a7207f4f0eb4b",
+                "name": "Expansão 216",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "suite",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "15",
+              "floor": "Térreo",
+              "locationName": "suite"
+            }
+          },
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "suite",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "15",
+              "floor": "Térreo",
+              "locationName": "suite"
+            }
+          }
+        ]
+      },
+      "sala": {
+        "config": {
+          "I2CKeyPad": "1",
+          "multiplexedKeyPad": 0,
+          "point": "8",
+          "pulser": "10",
+          "floor": "Térreo",
+          "locationName": "sala"
+        },
+        "products": [
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "sala",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "10",
+              "floor": "Térreo",
+              "locationName": "sala"
+            }
+          },
+          {
+            "productCode": "P7",
+            "_id": "61fbe6b8438a7207f4f0eb55",
+            "name": "Iluminação Dimmerizada",
+            "description": "Potência 160W/127V ",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "sala",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "10",
+              "floor": "Térreo",
+              "locationName": "sala"
+            }
+          },
+          {
+            "productCode": "P8",
+            "_id": "61fbe745438a7207f4f0eb59",
+            "name": "Iluminação L/D",
+            "description": "Mini Dimmer e Expansão 216 com carga de 1.",
+            "materials": [
+              {
+                "code": 52,
+                "_id": "61fbe4a5438a7207f4f0eb48",
+                "name": "Mini Dimmer",
+                "charge": 1
+              },
+              {
+                "code": 53,
+                "_id": "61fbe4ef438a7207f4f0eb4b",
+                "name": "Expansão 216",
+                "charge": 1
+              }
+            ],
+            "category": "Automação",
+            "group": "Iluminação",
+            "floor": "Térreo",
+            "homeLocationName": "sala",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "10",
+              "floor": "Térreo",
+              "locationName": "sala"
+            }
+          },
+          {
+            "productCode": "P9",
+            "_id": "61fbe857438a7207f4f0eb5f",
+            "name": "Persianas Motorizadas",
+            "description": "Persianas ",
+            "materials": [
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 1
+              },
+              {
+                "code": 54,
+                "_id": "61fbe5dc438a7207f4f0eb4e",
+                "name": "Expansão 212/24V",
+                "charge": 0.5
+              }
+            ],
+            "category": "Automação",
+            "group": "Acessórios",
+            "floor": "Térreo",
+            "homeLocationName": "sala",
+            "amount": 1,
+            "configLocation": {
+              "I2CKeyPad": "1",
+              "multiplexedKeyPad": 0,
+              "point": "8",
+              "pulser": "10",
+              "floor": "Térreo",
+              "locationName": "sala"
+            }
+          }
+        ]
+      }
+    }
+  },
+  "customer": {
+    "phone": "99999999999",
+    "name": "e2w",
+    "email": "e@w"
+  },
+  "createAt": {
+    "$date": "2022-02-17T21:09:30.441Z"
+  },
+  "code": "O1",
+  "__v": 0
+}
