68152678d1ffaa93a93d167cd1dda775f4efd212
Rosa, Genesis <mrgenesis@hotmail.com>
2022-03-10 17:40:14 -0400

    Adicionada o propriedade code no objeto user gerado no login. Assim essa informação vai constar no orçamento gerado.

--- modules/user/service/auth.js
+++ modules/user/service/auth.js
@@ -8,7 +8,7 @@ module.exports = function setContext(context) {
   async function auth(email, password) {
     const user = await context.model.user.findOne({ email }).select('+password');
     let tokenUser = { group: user.group, code: user.code, _id: user._id, name: user.name };
-    let responseUser = { email: user.email, name: user.name };
+    let responseUser = { email: user.email, name: user.name, code: user.code };
 
     if (!user) {
       throw this.createError('User not found/Password invalid.', 403);
