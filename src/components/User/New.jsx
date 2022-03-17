6b3a7e18d6be7539a84bbb1b3bb534759941cf96
Rosa, Genesis <mrgenesis@hotmail.com>
2022-03-17 10:27:03 -0400

    O cadastro de usuários corrigido para ser compatível com o sistema de grupo definido no beckend.

--- src/components/User/New.jsx
+++ src/components/User/New.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { TextField, Button, Box } from '@material-ui/core';
+import { TextField, Button, Box, RadioGroup, Radio } from '@material-ui/core';
 import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
 import { useForm } from 'react-hook-form';
-import FormGroup from '@material-ui/core/FormGroup';
-import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import FormControl from '@material-ui/core/FormControl';
@@ -23,40 +21,17 @@ export default function UserNew() {
   // Dados que serão salvos no Banco de dados
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [roles, setRoles] = React.useState([]);
+  const [group, setGroup] = React.useState([]);
+  const password = Math.random().toString(36).slice(2).toUpperCase().replace(/\w{3}/g, (c) => `${c}-`);
 
   // Configuração do funcionamento da página
-  const [basic, setBasic] = React.useState(false);
-  const [admin, setAdmin] = React.useState(false);
   const [runningApi, setRunningApi] = React.useState('stopped');
 
-  const handleBasic = (event) => {
-    setBasic(!basic);
-    updateRole('basic', (basic ? 'remove' : 'add'));
-  };
-  const handleAdmin = (event) => {
-    setAdmin(!admin);
-    updateRole('admin', (admin ? 'remove' : 'add'));
-  };
-  const updateRole = (role, action) => {
-    let copyRoles = roles;
-    if (action === 'add' && roles.indexOf(role) === -1) {
-      setRoles([...roles, role]);
-    }
-    else if (action === 'remove') {
-      //debugger;
-      if (copyRoles.indexOf(role) !== -1) {
-        copyRoles.splice(copyRoles.indexOf(role), 1);
-      }
-      setRoles(copyRoles);
-    }
-  }
   // Configuração da página
   function onSubmit(event) {
     saveUser({
       params: {
-        name, email, password, roles
+        name, email, password, group
       }, getResponse: () => { }, handleStatus: setRunningApi
     });
   }
@@ -92,36 +67,35 @@ export default function UserNew() {
               helperText={!!errors['email'] && 'Defina um e-mail.'}
             />
 
-            <TextField fullWidth disabled={(runningApi !== 'stopped')}
+            <TextField fullWidth disabled={true}
               name={'password'}
               label='Senha'
               type='text' margin='normal' value={password}
-              onChange={(event) => setPassword(event.target.value)}
               inputRef={register({ required: true })}
               error={!!errors['password']}
               helperText={!!errors['password'] && 'Defina uma senha'} />
 
 
             <FormControl component="fieldset">
-              <FormGroup aria-label="position" row>
+              <RadioGroup aria-label="position" row>
 
-                <FormControlLabel checked={basic}
-                  value="basic"
+                <FormControlLabel
+                  value="arquiteto"
                   disabled={(runningApi !== 'stopped')}
-                  onChange={handleBasic}
-                  control={<Checkbox color="primary" />}
+                  onClick={(e) => setGroup(e.target.value)}
+                  control={<Radio color="primary" />}
                   label="Básico"
                   labelPlacement="end"
                 />
-                <FormControlLabel checked={admin}
+                <FormControlLabel
                   disabled={(runningApi !== 'stopped')}
                   value="admin"
-                  onChange={handleAdmin}
-                  control={<Checkbox color="primary" />}
+                  onClick={(e) => setGroup(e.target.value)}
+                  control={<Radio color="primary" />}
                   label="Administrador"
                   labelPlacement="end"
                 />
-              </FormGroup>
+              </RadioGroup>
             </FormControl>
 
             <Button disabled={(runningApi !== 'stopped')} endIcon={<SaveOutlinedIcon />} type='submit' fullWidth>Salvar Material</Button>
