20b5f8b5b51f489dad7e92bd3359dfc86561f848
Rosa, Genesis <mrgenesis@hotmail.com>
2022-09-13 17:47:18 -0400

    corrigido bug na class de request

--- src/services/apis.js
+++ src/services/apis.js
@@ -5,18 +5,22 @@ export class Apis {
     method: 'get',
     headers: new Headers()
   };
-  setAuthHeaders(token) {
-    this.#options.headers.append('Authorization', token);
+  setAuthHeaders(token, type = 'Bearer') {
+    this.#options.headers.append('Authorization', `${type} ${token}`);
   }
-  set path(path) { this.#path = path.replace(/^\//, ''); }
   get path() { return this.#path; }
-  set urlBase(baseUrl) { this.#urlBase = baseUrl.replace(/\/$/, ''); }
   get urlBase() { return this.#urlBase; }
+  set path(path) { this.#path = this.removeSlash(path); }
+  set urlBase(baseUrl) { this.#urlBase = this.removeSlash(baseUrl, 'end'); }
   set options(opt) {
     this.#options = {
       ...this.#options,
       ...opt
     };
+  }
+  removeSlash(str, position = 'start') {
+    const remover = { start: s => s.replace(/^\//, ''), end: s => s.replace(/\/$/, ''), };
+    return remover[position](str);
   }
   secureStringifyOfBody(body) {
     if(typeof body === 'string') {
@@ -36,8 +40,10 @@ export class Apis {
     this.#options.method = method;
   }
   request(path = this.#path, options = {}) {
+    const mergedOptions = { ...this.#options, ...options };
+    path = this.removeSlash(path);
     return new Promise((resolve, reject) => {
-      window.fetch(this.endpoint(path), { ...this.#options, ...options })
+      window.fetch(this.endpoint(path), mergedOptions)
       .then(resolve)
       .catch(reject);
     });
