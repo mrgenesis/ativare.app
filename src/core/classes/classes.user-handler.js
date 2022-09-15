b46145b96f05d78fa9d5726fab99b3762855aaf0
Rosa, Genesis <mrgenesis@hotmail.com>
2022-09-15 09:59:37 -0400

    Criada uma classe que lida com as permissoes de todos os acessos ao sistema baseado no grupo/recurso

---
+++ src/core/classes/classes.user-handler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+class UserHendler {
+  #token;
+  #adicionalsResources;
+  constructor(token) {
+    this.#token = token;
+  }
+  get id() {
+    return this.#token.oid;
+  }
+  get role() {
+    return this.#token.roles[0];
+  }
+  isId(id) {
+    return id === this.id;
+  }
+  isAdmin() {
+    return this.#token.roles[0] === 'Admin.Materiais_e_Orcamentos';
+  }
+  comumUserGrant(sourceArn, group) {
+    if (!sourceArn) return false;
+    if (!group) return false;
+    if (!group[sourceArn]) return false;
+    return true;
+  }
+  isGranted(src, groups) {
+    if(this.isAdmin()) {
+      this.#adicionalsResources = groups[this.role]['*'];
+      return true;
+    }
+    if (this.comumUserGrant(src.arn, groups[this.role])) {
+      this.#adicionalsResources = groups[this.role][src.arn];
+      return true;
+    }
+    return false;
+  }
+  isGrantedResource(resourceName) {
+    return this.isAdmin() || this.#adicionalsResources.indexOf(resourceName) > -1;
+  }
+}
+
+module.exports = UserHendler;
