f66162d9fe24dd981ef2c9a0c24f230ad4a84d47
Rosa, Genesis <mrgenesis@hotmail.com>
2022-09-18 07:23:13 -0400

    correção de bugs

--- src/config/aad.js
+++ src/config/aad.js
@@ -1,13 +1,13 @@
 
 const apiConfig = {
-  urlBase: process.env.REACT_APP_URL_API_BASE || "http://localhost:3000/"
+  urlBase: process.env.REACT_APP_URL_BASE_API
 }
 
 const msalAadClientConfig = {
   auth: {
     clientId: "ce9c5f25-9096-4fd9-985f-4f1718fc7f53",
     authority: "https://login.microsoftonline.com/2bd70d8a-6f4e-4cc1-aa00-469ce9c54770",
-    redirectUri: process.env.REACT_APP_URL_FRRONT_BASE || "http://localhost:3001/",
+    redirectUri: process.env.PUBLIC_URL,
   },
   cache: {
     cacheLocation: "localStorage",
